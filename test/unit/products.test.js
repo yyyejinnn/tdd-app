e842ab9c06aa77a43480bd7d03c189b9e3f42f87
yyyejinnn <lyjin2007@gmail.com>
2023-07-31 17:07:50 +0900

    Section1: Jest 개념 및 사용법

---
+++ test/unit/products.test.js
@@ -0,0 +1,9 @@
+describe('Calculation', () => {
+    test('two plus tow is four', () => {
+        expect(2 + 2).toBe(4);
+    })
+
+    test('two plus tow is not five', () => {
+        expect(2 + 2).not.toBe(5);
+    })
+})
