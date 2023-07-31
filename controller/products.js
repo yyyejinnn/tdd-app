1173e1643d0dc672c2856c24bbaf8793724a8aec
yyyejinnn <lyjin2007@gmail.com>
2023-07-31 17:36:54 +0900

    Section2: createProduct 함수 생성

--- controller/products.js
+++ controller/products.js
@@ -1,3 +1,9 @@
-exports.hello = (req, res)=> {
-    res.send('안녕하세요');
+const productModel = require('../models/Product');
+
+exports.createProduct = () => {
+    productModel.create();
 };
+
+// exports.hello = (req, res)=> {
+//     res.send('안녕하세요');
+// };
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,9 +1,26 @@
-describe('Calculation', () => {
-    test('two plus tow is four', () => {
-        expect(2 + 2).toBe(4);
-    })
+const productController = require('../.././controller/products');
+const productModel = require('../../models/Product');
 
-    test('two plus tow is not five', () => {
-        expect(2 + 2).not.toBe(5);
+productModel.create = jest.fn();        // 2-3. model에 의존하지 않기 위해 mock 함수 생성
+
+describe('Product Controller Create', () => {
+    it('should have a createProcduct function', () => {
+        expect(typeof productController.createProduct).toBe('function');     // 1. 해당 함수가 실제로 존재하는지 확인
+    });
+
+    it('should call ProductModel.create', () => {
+        productController.createProduct();              // 2-1. createProduct() 함수 호출될 때
+        expect(productModel.create).toBeCalled();       // 2-2. 해당 model의 create 함수도 호출되는지 확인
     })
-})
+});
+
+
+// describe('Calculation', () => {
+//     test('two plus tow is four', () => {
+//         expect(2 + 2).toBe(4);
+//     })
+
+//     test('two plus tow is not five', () => {
+//         expect(2 + 2).not.toBe(5);
+//     })
+// })
